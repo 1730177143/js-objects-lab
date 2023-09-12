f1b4eb50d3b566bad2ef8a8e6758cd6569a459a0
liulong <1730177143@qq.com>
2023-09-12 11:31:31 +0100

    Arrays.

--- fundamentals.js
+++ fundamentals.js
@@ -1,5 +1,10 @@
 const aCar = {
     owner : "Joe Bloggs",
+    address : '3 Walkers Lane',
+    previous_owners: [
+       {name: "Pat Smith", address: "1 Main Street",},
+       {name:"Sheila Dwyer" ,address: "2 High Street"}
+      ],
     type :{
         make:'Toyota',
         model:'Corolla',
@@ -19,3 +24,4 @@ const aCar = {
   console.log(aCar.owner + ' drives a ' + aCar.type.make);
   console.log('Reg.='+aCar.registration.year+'-'+aCar.registration.countyCode+'-'+aCar.registration.number);
 console.log('It is a '+aCar.colour.exterior+' car,'+aCar.mileage+ ' mileage, with '+aCar.colour.interior.texture+' interior');
+console.log(console.log('First owner : ' + aCar.previous_owners[0].name+'_'+aCar.previous_owners[0].address ));
